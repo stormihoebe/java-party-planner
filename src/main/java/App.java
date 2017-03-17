c2006d48dfc9832263873013e3f61bfb3d12e2de
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-03-17 08:14:02 -0700

    add initial files

---
+++ src/main/java/App.java
@@ -0,0 +1,9 @@
+import java.io.Console;
+
+public class App{
+	public static void main(String[] args) {
+		Console myConsole = System.console();
+		Party newParty = new Party();
+
+	}
+}
---
+++ src/main/java/Party.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+public class Party{
+  public Integer mNumberOfGuests;
+
+
+  public Integer partyPrice (){
+    return 0;
+  }
+}
